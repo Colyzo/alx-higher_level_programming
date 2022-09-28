e35dcf4ee3b15a99729cc0e9936155aa3a65f489
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-09-28 14:56:58 -0700

    Update 3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    intersection = []
+    if set_1 is not None and set_2 is not None:
+        intersection = set_1 & set_2
+        return intersection
